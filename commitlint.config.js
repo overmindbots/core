dfe265cfae7e5bfab5fd82278f5b061f8d8c9e05
Rafael Vidaurre <narzerus@gmail.com>
2018-07-10 15:03:37 -0400

    fix: fix incorrect commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,6 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'header-max-length': [2, 'always', [72]],
-    'footer-leading-blank': false,
+    'footer-leading-blank': [0],
   },
 };
