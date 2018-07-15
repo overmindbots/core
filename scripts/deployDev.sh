9939015f217fe5976d318cac7fa8953cf8f7b202
Rafael Vidaurre <narzerus@gmail.com>
2018-07-14 20:52:34 -0400

    WIP: wip docker builds
    affects: @overmindbots/service-referral-ranks-invites

---
+++ scripts/deployDev.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+kubectx minikube
+
+lerna bootstrap
+
+rm -rf k8s-generated
+mkdir k8s-generated
+touch k8s-generated/.gitkeep
+
+{
+  helm install --name bot-alchemy-mongodb stable/mongodb --namespace overmindbots --set mongodbUsername=dev,mongodbPassword=dev,mongodbDatabase=overmindbots
+} || {
+  echo "Already installed mongodb"
+}
+
+{
+  kubectl create namespace overmindbots
+} || {
+  echo "Did not create overmindbots namespace"
+}
+
+# === Build images === #
+
+serviceReferralRanksInvites=service-referral-ranks-invites:develop
+
+docker build -t $serviceReferralRanksInvites -f packages/service-referral-ranks-invites/Dockerfile .
+
+{
+  kubectl apply -f ./k8s-generated
+} || {
+  echo "Did not apply"
+}
