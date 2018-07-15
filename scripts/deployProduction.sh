95496cbd09d7a07082ae006a5944b21c83a12183
Rafael Vidaurre <narzerus@gmail.com>
2018-07-15 17:24:36 -0400

    WIP: wip

--- scripts/deployProduction.sh
+++ scripts/deployProduction.sh
@@ -2,14 +2,13 @@
 set -e
 
 # === Build Kubernetes templates === #
-yarn build-templates
+node ./scripts/buildTemplates.js
 
 # === Build images === #
 serviceReferralRanksInvitesImgUrl=gcr.io/${GOOGLE_PROJECT_ID}/service-referral-ranks-invites:${CIRCLE_BRANCH}-${CIRCLE_BUILD_NUM}
 
 docker build -t $serviceReferralRanksInvitesImgUrl -f packages/service-referral-ranks-invites/Dockerfile .
 
-node ./scripts/buildTemplates.js
 
 {
   kubectl apply -f ./k8s-generated
