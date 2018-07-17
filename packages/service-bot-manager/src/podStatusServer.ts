5198855e860bb549d2bb9d04df4346f7fee37af0
Sebastian Vera <u.verainf@gmail.com>
2018-07-17 17:32:33 -0400

    style: add todo
    affects: @overmindbots/service-bot-manager

--- packages/service-bot-manager/src/podStatusServer.ts
+++ packages/service-bot-manager/src/podStatusServer.ts
@@ -2,6 +2,7 @@ import { PodStatusServer } from '@overmindbots/shared-utils/podStatusServer';
 
 export const podStatusServer = new PodStatusServer();
 
+// TODO: make this testeable
 if (process.env.NODE_ENV !== 'test') {
   podStatusServer.start();
 }
