d296c805685df8bc8d238b2a3197a3e3d29ea3f0
Sebastian Vera <u.verainf@gmail.com>
2018-07-17 17:23:41 -0400

    chore: add jest dependency and base config
    affects: @overmindbots/bot-referral-ranks, @overmindbots/discord.js-command-manager,
    @overmindbots/service-bot-manager, @overmindbots/service-referral-ranks-invites,
    @overmindbots/shared-models, @overmindbots/shared-utils

---
+++ jest.base.js
@@ -0,0 +1,18 @@
+module.exports = {
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  testMatch: ['**/test/*.+(ts|tsx|js)'],
+  testPathIgnorePatterns: ['/node_modules/', 'setupTests.ts'],
+  moduleFileExtensions: ['ts', 'js', 'node'],
+  moduleNameMapper: {
+    '~/(.+)': '<rootDir>/src/$1',
+    '@overmindbots/shared-utils(.+)':
+      '<rootDir>/node_modules/@overmindbots/shared-utils/src/$1',
+    '@overmindbots/shared-models(.+)':
+      '<rootDir>/node_modules/@overmindbots/shared-models/src/$1',
+    '@overmindbots/discord.js-command-manager(.+)':
+      '<rootDir>/node_modules/@overmindbots/discord.js-command-manager/src/$1',
+  },
+  setupTestFrameworkScriptFile: '<rootDir>/test/setupTests.ts',
+};
