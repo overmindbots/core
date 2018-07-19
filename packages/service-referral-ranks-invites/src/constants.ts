b8dfef2bd2f836ea4d25ae8c5e6e876a43abc2d0
Jeremias Diaz <monitz87@gmail.com>
2018-07-18 20:41:35 -0400

    fix: fix faulty implementation of assignment logic
    affects: @overmindbots/service-referral-ranks-invites
    
    Added a CHUNK_SIZE env variable to regulate how many guilds are fetched at the sime time on
    startup
    Fixed the noNewUsesCount not being reset when a guild's loop was idle and a new member
    joined
    Fixed an error produced when trying to assign a referral from an invite with no inviter.
    These invites are now ignored
    When members joined a guild without the proper permissions, its member
    queue would keep growing indefinitely. This introduced a potential memory issue. Now the queue is
    cleared each time a fetch returns a permission error

--- packages/service-referral-ranks-invites/src/constants.ts
+++ packages/service-referral-ranks-invites/src/constants.ts
@@ -11,8 +11,12 @@ if (!process.env.SHARD_ID) {
 if (!process.env.MONGODB_URI) {
   throw new Error('Missing env variable MONGODB_URI');
 }
+if (!process.env.CHUNK_SIZE) {
+  throw new Error('Missing env variable CHUNK_SIZE');
+}
 
 export const BOT_TOKEN = process.env.BOT_TOKEN;
 export const SHARD_ID = parseInt(process.env.SHARD_ID, 10);
 export const TOTAL_SHARDS = parseInt(process.env.TOTAL_SHARDS, 10);
 export const MONGODB_URI = process.env.MONGODB_URI;
+export const CHUNK_SIZE = parseInt(process.env.CHUNK_SIZE, 10);
