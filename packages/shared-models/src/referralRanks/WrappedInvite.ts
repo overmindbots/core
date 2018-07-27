a414a4eea762df208d6172b74c01c813e37a4ccb
Jeremias Diaz <monitz87@gmail.com>
2018-07-26 21:40:59 -0400

    feat: implement WrappedInvite model
    affects: @overmindbots/shared-models

---
+++ packages/shared-models/src/referralRanks/WrappedInvite.ts
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+export interface WrappedInviteDocument extends mongoose.Document {
+  guildDiscordId: string;
+  code: string;
+  url: string;
+}
+export interface WrappedInviteModel
+  extends mongoose.Model<WrappedInviteDocument> {}
+
+const schema = new mongoose.Schema({
+  code: {
+    required: true,
+    type: String,
+  },
+  guildDiscordId: {
+    required: true,
+    type: String,
+  },
+  url: {
+    required: true,
+    type: String,
+  },
+});
+
+schema.index({ guildDiscordId: 1 });
+schema.index({ code: 1 }, { unique: true });
+
+/**
+ * Represents a wrapped invite link for a guild
+ */
+export const Invite = mongoose.model<WrappedInviteDocument, WrappedInviteModel>(
+  'Bot-ReferralRanks-WrappedInvite',
+  schema
+);
