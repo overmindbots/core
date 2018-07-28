5dd20056aec9abc1c02fa12ffe70d88bcc424db2
Jeremias Diaz <monitz87@gmail.com>
2018-07-28 12:51:19 -0400

    fix: add CertainReferral export in index
    affects: @overmindbots/shared-models

--- packages/shared-models/src/referralRanks/index.ts
+++ packages/shared-models/src/referralRanks/index.ts
@@ -3,4 +3,5 @@ export { InviteUse } from './InviteUse';
 export { Invite } from './Invite';
 export { Control } from './Control';
 export { Referral } from './Referral';
+export { CertainReferral } from './CertainReferral';
 export { WrappedInvite } from './WrappedInvite';
