1e7f59c900abe1e6a831af96c8a10ea18e53e6fc
Rafael Vidaurre <narzerus@gmail.com>
2018-08-01 16:09:47 -0400

    refactor: rename getMemberInviteLink to getUserInviteLinkUrl
    affects: @overmindbots/shared-utils

--- packages/shared-utils/src/botReferralRanks.ts
+++ packages/shared-utils/src/botReferralRanks.ts
@@ -1,6 +1,6 @@
 import { APP_REFERRAL_RANKS_INVITE_WRAPPER_URL } from '@overmindbots/shared-utils/constants';
 
-export function getMemberInviteLink(
+export function getUserInviteLinkUrl(
   userDiscordId: string,
   guildDiscordId: string
 ) {
