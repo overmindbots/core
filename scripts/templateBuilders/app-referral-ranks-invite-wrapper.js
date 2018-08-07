c5b437fd8cd905107d66fc32219f12a7ce441ce4
Rafael Vidaurre <narzerus@gmail.com>
2018-08-07 18:38:35 -0400

    feat: implement new services deployments
    add kubernetes file templates for new services

---
+++ scripts/templateBuilders/app-referral-ranks-invite-wrapper.js
@@ -0,0 +1,31 @@
+require('./shared/init');
+
+const _ = require('lodash');
+const path = require('path');
+
+const utils = require('./shared/utils');
+
+const totalShards = process.env.APP_REFERRAL_RANKS_INVITE_WRAPPER_TOTAL_SHARDS;
+if (!totalShards) {
+  throw new Error(
+    `Environment variable missing: APP_REFERRAL_RANKS_INVITE_WRAPPER_TOTAL_SHARDS`
+  );
+}
+
+const packageName = path.basename(__filename).split('.js')[0];
+const templateValues = utils.getBaseTemplateData();
+
+// Generate one deployment per shardId
+_.range(0, totalShards).forEach(shardId => {
+  const extendedTemplateValues = Object.assign({}, templateValues, {
+    shardId,
+  });
+
+  utils.buildTemplate(
+    packageName,
+    extendedTemplateValues,
+    `${packageName}-${shardId}`
+  );
+});
+
+console.log(`====> Successfully built template for ${packageName}`);
---
+++ scripts/templateBuilders/bot-referral-ranks-fulfillment-service.js
@@ -0,0 +1,32 @@
+require('./shared/init');
+
+const _ = require('lodash');
+const path = require('path');
+
+const utils = require('./shared/utils');
+
+const totalShards =
+  process.env.BOT_REFERRAL_RANKS_FULFILLMENT_SERVICE_TOTAL_SHARDS;
+if (!totalShards) {
+  throw new Error(
+    `Environment variable missing: BOT_REFERRAL_RANKS_FULFILLMENT_SERVICE_TOTAL_SHARDS`
+  );
+}
+
+const packageName = path.basename(__filename).split('.js')[0];
+const templateValues = utils.getBaseTemplateData();
+
+// Generate one deployment per shardId
+_.range(0, totalShards).forEach(shardId => {
+  const extendedTemplateValues = Object.assign({}, templateValues, {
+    shardId,
+  });
+
+  utils.buildTemplate(
+    packageName,
+    extendedTemplateValues,
+    `${packageName}-${shardId}`
+  );
+});
+
+console.log(`====> Successfully built template for ${packageName}`);
