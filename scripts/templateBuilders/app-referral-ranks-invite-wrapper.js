e034bc918c2314692b260dfca1a397303c8f77f8
Rafael Vidaurre <narzerus@gmail.com>
2018-08-12 18:59:51 -0300

    fix: correct bad environment variables definitions and usage
    affects: @overmindbots/app-referral-ranks-invite-wrapper,
    @overmindbots/bot-referral-ranks-fulfillment-service, @overmindbots/shared-utils

--- scripts/templateBuilders/app-referral-ranks-invite-wrapper.js
+++ scripts/templateBuilders/app-referral-ranks-invite-wrapper.js
@@ -1,32 +1,34 @@
 require('./shared/init');
 
-const _ = require('lodash');
 const path = require('path');
-
 const utils = require('./shared/utils');
-
-const totalShards = process.env.APP_REFERRAL_RANKS_INVITE_WRAPPER_TOTAL_SHARDS;
-if (!totalShards) {
-  throw new Error(
-    `Environment variable missing: APP_REFERRAL_RANKS_INVITE_WRAPPER_TOTAL_SHARDS`
-  );
-}
 
 const packageName = path.basename(__filename).split('.js')[0];
 const templateValues = utils.getBaseTemplateData();
+const stage = utils.getDeploymentStage();
+let url;
+
+switch (stage) {
+  case 'development': {
+    url = 'localhost';
+    break;
+  }
+  case 'staging': {
+    url = 'www.referralranks.org'; // Temporary URL
+    break;
+  }
+  case 'production': {
+    url = 'www.referralranks.com';
+    break;
+  }
+}
+
+const extendedTemplateValues = {
+  ...templateValues,
+  url,
+};
 
 // Generate one deployment per shardId
-_.range(0, totalShards).forEach(shardId => {
-  const extendedTemplateValues = Object.assign({}, templateValues, {
-    shardId,
-    totalShards,
-  });
-
-  utils.buildTemplate(
-    packageName,
-    extendedTemplateValues,
-    `${packageName}-${shardId}`
-  );
-});
+utils.buildTemplate(packageName, extendedTemplateValues, `${packageName}`);
 
 console.log(`====> Successfully built template for ${packageName}`);
